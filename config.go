b4e3cb7d8aa9eb31e3dbf25dc6c4f75a639ee54c
Evgeniy Udodov <flr.null@gmail.com>
2016-12-02 23:01:43 +0300

    gofmt && golint

--- config.go
+++ config.go
@@ -2,179 +2,179 @@ package main
 
 // MetricConfig is one line on graph
 type MetricConfig struct {
-    id              string
-    label           string
-    metricType      string
-    drawType        string
-    color           string
-    clickHouseEvent string
+	id              string
+	label           string
+	metricType      string
+	drawType        string
+	color           string
+	clickHouseEvent string
 }
 
 // Graph itself
 type Graph struct {
-    graphTitle    string
-    graphCategory string
-    graphInfo     string
-    graphLabel    string
-    graphPeriod   string
-    graphArgs     string
-    isPercent     bool
-    metrics       []MetricConfig
+	graphTitle    string
+	graphCategory string
+	graphInfo     string
+	graphLabel    string
+	graphPeriod   string
+	graphArgs     string
+	isPercent     bool
+	metrics       []MetricConfig
 }
 
 // AvailableGraphs is list of available graphs
 var AvailableGraphs = map[string]Graph{
-    "queries": {
-        graphTitle:    "ClickHouse queries",
-        graphCategory: "clickhouse",
-        graphInfo:     "Values received from ClickHouse system.events table",
-        graphLabel:    "queries / second",
-        graphPeriod:   "second",
-        graphArgs:     "--lower-limit 0",
-        metrics: []MetricConfig{
-            {
-                id:              "select",
-                label:           "Selects",
-                metricType:      "DERIVE",
-                drawType:        "AREA",
-                color:           "COLOUR0",
-                clickHouseEvent: "SelectQuery",
-            },
-            {
-                id:              "insert",
-                label:           "Inserts",
-                metricType:      "DERIVE",
-                drawType:        "STACK",
-                color:           "COLOUR1",
-                clickHouseEvent: "InsertQuery",
-            },
-        },
-    },
-    "files": {
-        graphTitle:    "ClickHouse files",
-        graphCategory: "clickhouse",
-        graphInfo:     "Values received from ClickHouse system.events table",
-        graphLabel:    "operations / second",
-        graphPeriod:   "second",
-        graphArgs:     "--lower-limit 0",
-        metrics: []MetricConfig{
-            {
-                id:              "file_open",
-                label:           "Opens",
-                metricType:      "DERIVE",
-                drawType:        "AREA",
-                color:           "COLOUR0",
-                clickHouseEvent: "FileOpen",
-            },
-            {
-                id:              "seek",
-                label:           "Seeks",
-                metricType:      "DERIVE",
-                drawType:        "STACK",
-                color:           "COLOUR1",
-                clickHouseEvent: "Seek",
-            },
-        },
-    },
-    "cache": {
-        graphTitle:    "ClickHouse cache",
-        graphCategory: "clickhouse",
-        graphInfo:     "Values received from ClickHouse system.events table",
-        graphLabel:    "per second",
-        graphPeriod:   "second",
-        graphArgs:     "-u 100 -l 0 -r --base 1000",
-        isPercent:     true,
-        metrics: []MetricConfig{
-            {
-                id:              "hits",
-                label:           "hit ratio",
-                metricType:      "GAUGE",
-                drawType:        "AREA",
-                color:           "COLOUR0",
-                clickHouseEvent: "MarkCacheHits",
-            },
-            {
-                id:              "misses",
-                label:           "miss ratio",
-                metricType:      "GAUGE",
-                drawType:        "STACK",
-                color:           "COLOUR1",
-                clickHouseEvent: "MarkCacheMisses",
-            },
-        },
-    },
-    "zookeeper": {
-        graphTitle:    "ClickHouse Zookeeper transactions",
-        graphCategory: "clickhouse",
-        graphInfo:     "Values received from ClickHouse system.events table",
-        graphLabel:    "operations / second",
-        graphPeriod:   "second",
-        graphArgs:     "--lower-limit 0",
-        metrics: []MetricConfig{
-            {
-                id:              "ZooKeeperGetChildren",
-                label:           "ZooKeeperGetChildren",
-                metricType:      "DERIVE",
-                drawType:        "AREA",
-                color:           "COLOUR0",
-                clickHouseEvent: "ZooKeeperGetChildren",
-            },
-            {
-                id:              "ZooKeeperCreate",
-                label:           "ZooKeeperCreate",
-                metricType:      "DERIVE",
-                drawType:        "STACK",
-                color:           "COLOUR1",
-                clickHouseEvent: "ZooKeeperCreate",
-            },
-            {
-                id:              "ZooKeeperRemove",
-                label:           "ZooKeeperRemove",
-                metricType:      "DERIVE",
-                drawType:        "STACK",
-                color:           "COLOUR2",
-                clickHouseEvent: "ZooKeeperRemove",
-            },
-            {
-                id:              "ZooKeeperExists",
-                label:           "ZooKeeperExists",
-                metricType:      "DERIVE",
-                drawType:        "STACK",
-                color:           "COLOUR3",
-                clickHouseEvent: "ZooKeeperExists",
-            },
-            {
-                id:              "ZooKeeperGet",
-                label:           "ZooKeeperGet",
-                metricType:      "DERIVE",
-                drawType:        "STACK",
-                color:           "COLOUR4",
-                clickHouseEvent: "ZooKeeperGet",
-            },
-            {
-                id:              "ZooKeeperSet",
-                label:           "ZooKeeperSet",
-                metricType:      "DERIVE",
-                drawType:        "STACK",
-                color:           "COLOUR5",
-                clickHouseEvent: "ZooKeeperSet",
-            },
-            {
-                id:              "ZooKeeperMulti",
-                label:           "ZooKeeperMulti",
-                metricType:      "DERIVE",
-                drawType:        "STACK",
-                color:           "COLOUR6",
-                clickHouseEvent: "ZooKeeperMulti",
-            },
-            {
-                id:              "ZooKeeperExceptions",
-                label:           "ZooKeeperExceptions",
-                metricType:      "DERIVE",
-                drawType:        "STACK",
-                color:           "COLOUR7",
-                clickHouseEvent: "ZooKeeperExceptions",
-            },
-        },
-    },
+	"queries": {
+		graphTitle:    "ClickHouse queries",
+		graphCategory: "clickhouse",
+		graphInfo:     "Values received from ClickHouse system.events table",
+		graphLabel:    "queries / second",
+		graphPeriod:   "second",
+		graphArgs:     "--lower-limit 0",
+		metrics: []MetricConfig{
+			{
+				id:              "select",
+				label:           "Selects",
+				metricType:      "DERIVE",
+				drawType:        "AREA",
+				color:           "COLOUR0",
+				clickHouseEvent: "SelectQuery",
+			},
+			{
+				id:              "insert",
+				label:           "Inserts",
+				metricType:      "DERIVE",
+				drawType:        "STACK",
+				color:           "COLOUR1",
+				clickHouseEvent: "InsertQuery",
+			},
+		},
+	},
+	"files": {
+		graphTitle:    "ClickHouse files",
+		graphCategory: "clickhouse",
+		graphInfo:     "Values received from ClickHouse system.events table",
+		graphLabel:    "operations / second",
+		graphPeriod:   "second",
+		graphArgs:     "--lower-limit 0",
+		metrics: []MetricConfig{
+			{
+				id:              "file_open",
+				label:           "Opens",
+				metricType:      "DERIVE",
+				drawType:        "AREA",
+				color:           "COLOUR0",
+				clickHouseEvent: "FileOpen",
+			},
+			{
+				id:              "seek",
+				label:           "Seeks",
+				metricType:      "DERIVE",
+				drawType:        "STACK",
+				color:           "COLOUR1",
+				clickHouseEvent: "Seek",
+			},
+		},
+	},
+	"cache": {
+		graphTitle:    "ClickHouse cache",
+		graphCategory: "clickhouse",
+		graphInfo:     "Values received from ClickHouse system.events table",
+		graphLabel:    "per second",
+		graphPeriod:   "second",
+		graphArgs:     "-u 100 -l 0 -r --base 1000",
+		isPercent:     true,
+		metrics: []MetricConfig{
+			{
+				id:              "hits",
+				label:           "hit ratio",
+				metricType:      "GAUGE",
+				drawType:        "AREA",
+				color:           "COLOUR0",
+				clickHouseEvent: "MarkCacheHits",
+			},
+			{
+				id:              "misses",
+				label:           "miss ratio",
+				metricType:      "GAUGE",
+				drawType:        "STACK",
+				color:           "COLOUR1",
+				clickHouseEvent: "MarkCacheMisses",
+			},
+		},
+	},
+	"zookeeper": {
+		graphTitle:    "ClickHouse Zookeeper transactions",
+		graphCategory: "clickhouse",
+		graphInfo:     "Values received from ClickHouse system.events table",
+		graphLabel:    "operations / second",
+		graphPeriod:   "second",
+		graphArgs:     "--lower-limit 0",
+		metrics: []MetricConfig{
+			{
+				id:              "ZooKeeperGetChildren",
+				label:           "ZooKeeperGetChildren",
+				metricType:      "DERIVE",
+				drawType:        "AREA",
+				color:           "COLOUR0",
+				clickHouseEvent: "ZooKeeperGetChildren",
+			},
+			{
+				id:              "ZooKeeperCreate",
+				label:           "ZooKeeperCreate",
+				metricType:      "DERIVE",
+				drawType:        "STACK",
+				color:           "COLOUR1",
+				clickHouseEvent: "ZooKeeperCreate",
+			},
+			{
+				id:              "ZooKeeperRemove",
+				label:           "ZooKeeperRemove",
+				metricType:      "DERIVE",
+				drawType:        "STACK",
+				color:           "COLOUR2",
+				clickHouseEvent: "ZooKeeperRemove",
+			},
+			{
+				id:              "ZooKeeperExists",
+				label:           "ZooKeeperExists",
+				metricType:      "DERIVE",
+				drawType:        "STACK",
+				color:           "COLOUR3",
+				clickHouseEvent: "ZooKeeperExists",
+			},
+			{
+				id:              "ZooKeeperGet",
+				label:           "ZooKeeperGet",
+				metricType:      "DERIVE",
+				drawType:        "STACK",
+				color:           "COLOUR4",
+				clickHouseEvent: "ZooKeeperGet",
+			},
+			{
+				id:              "ZooKeeperSet",
+				label:           "ZooKeeperSet",
+				metricType:      "DERIVE",
+				drawType:        "STACK",
+				color:           "COLOUR5",
+				clickHouseEvent: "ZooKeeperSet",
+			},
+			{
+				id:              "ZooKeeperMulti",
+				label:           "ZooKeeperMulti",
+				metricType:      "DERIVE",
+				drawType:        "STACK",
+				color:           "COLOUR6",
+				clickHouseEvent: "ZooKeeperMulti",
+			},
+			{
+				id:              "ZooKeeperExceptions",
+				label:           "ZooKeeperExceptions",
+				metricType:      "DERIVE",
+				drawType:        "STACK",
+				color:           "COLOUR7",
+				clickHouseEvent: "ZooKeeperExceptions",
+			},
+		},
+	},
 }
--- main.go
+++ main.go
@@ -1,92 +1,92 @@
 package main
 
 import (
-    "fmt"
-    "github.com/roistat/go-clickhouse"
-    "os"
-    "strings"
+	"fmt"
+	"github.com/roistat/go-clickhouse"
+	"os"
+	"strings"
 )
 
 func main() {
-    graphName, action := parseOptions()
-    graph, ok := AvailableGraphs[graphName]
-    if !ok {
-        fmt.Printf("Invalid graph name: %s", graphName)
-        os.Exit(1)
-    }
+	graphName, action := parseOptions()
+	graph, ok := AvailableGraphs[graphName]
+	if !ok {
+		fmt.Printf("Invalid graph name: %s", graphName)
+		os.Exit(1)
+	}
 
-    if action == "config" {
-        renderGraphConfig(graph)
-    } else {
-        renderGraphData(graph)
-    }
+	if action == "config" {
+		renderGraphConfig(graph)
+	} else {
+		renderGraphData(graph)
+	}
 }
 
 func renderGraphConfig(g Graph) {
-    fmt.Printf(`graph_title %s
+	fmt.Printf(`graph_title %s
 graph_category %s
 graph_info %s
 graph_vlabel %s
 graph_period %s
 graph_args %s
 `, g.graphTitle, g.graphCategory, g.graphInfo, g.graphLabel, g.graphPeriod, g.graphArgs)
-    for _, m := range g.metrics {
-        fmt.Printf(`
+	for _, m := range g.metrics {
+		fmt.Printf(`
 %s.label %s
 %s.type %s
 %s.min 0
 %s.draw %s
 %s.colour %s
 `, m.id, m.label, m.id, m.metricType, m.id, m.id, m.drawType, m.id, m.color)
-    }
+	}
 }
 
 func renderGraphData(g Graph) {
-    stats := loadClickHouseStats()
-    total := 0
-    if g.isPercent {
-        for _, m := range g.metrics {
-            total = total + stats[m.clickHouseEvent]
-        }
-    }
-    for _, m := range g.metrics {
-        value := stats[m.clickHouseEvent]
-        if g.isPercent {
-            calcValue := float64(value) / float64(total) * float64(100)
-            formattedValue := fmt.Sprintf("%.2f", calcValue)
-            fmt.Printf("%s.value %s\n", m.id, formattedValue)
-        } else {
-            fmt.Printf("%s.value %d\n", m.id, value)
-        }
-    }
+	stats := loadClickHouseStats()
+	total := 0
+	if g.isPercent {
+		for _, m := range g.metrics {
+			total = total + stats[m.clickHouseEvent]
+		}
+	}
+	for _, m := range g.metrics {
+		value := stats[m.clickHouseEvent]
+		if g.isPercent {
+			calcValue := float64(value) / float64(total) * float64(100)
+			formattedValue := fmt.Sprintf("%.2f", calcValue)
+			fmt.Printf("%s.value %s\n", m.id, formattedValue)
+		} else {
+			fmt.Printf("%s.value %d\n", m.id, value)
+		}
+	}
 
 }
 
 func loadClickHouseStats() map[string]int {
-    conn := clickhouse.NewConn("localhost:8123", clickhouse.NewHttpTransport())
-    query := clickhouse.NewQuery("SELECT event, value FROM system.events")
-    iterator := query.Iter(conn)
-    var (
-        event string
-        value int
-    )
-    result := map[string]int{}
-    for iterator.Scan(&event, &value) {
-        result[event] = value
-    }
-    return result
+	conn := clickhouse.NewConn("localhost:8123", clickhouse.NewHttpTransport())
+	query := clickhouse.NewQuery("SELECT event, value FROM system.events")
+	iterator := query.Iter(conn)
+	var (
+		event string
+		value int
+	)
+	result := map[string]int{}
+	for iterator.Scan(&event, &value) {
+		result[event] = value
+	}
+	return result
 }
 
 func parseOptions() (string, string) {
-    graphName := "queries"
-    action := "data"
-    args := os.Args
-    if len(args) > 1 {
-        action = args[1]
-    }
-    nameParts := strings.Split(args[0], "_")
-    if len(nameParts) > 1 {
-        graphName = nameParts[1]
-    }
-    return graphName, action
+	graphName := "queries"
+	action := "data"
+	args := os.Args
+	if len(args) > 1 {
+		action = args[1]
+	}
+	nameParts := strings.Split(args[0], "_")
+	if len(nameParts) > 1 {
+		graphName = nameParts[1]
+	}
+	return graphName, action
 }
